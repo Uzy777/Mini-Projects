f69020c16cf5bc36089e5039b2c3561e0acff872
Uzy777 <usmon.h@outlook.com>
2024-12-18 19:53:18 +0000

    Update modular design where each player rolls the die to see who goes first in the game based on highest number, unfinished

--- Pig Game/main.py
+++ Pig Game/main.py
@@ -1,15 +1,24 @@
 import time
+import random
 
 
 # Main Menu
 def main_menu():
     while True:
         print("\nWelcome .. . . . .. . ")
-        print("--- Game Settings ---")
+        print("------ Game Modes ------")
         print("1: Single Player (Not Available)")
         print("2: Two Players")
-        print("--- Sound Settings ---")
-        print("S: Sound Toggle (on/off)")
+        print("3: Three Players (Not Available)")
+        print("4: Four Players (Not Available)")
+        print("5: Five or More Players (Not Available)")
+        print("------ Gameplay Settings ------")
+        print("A: Auto Roll (off) (Not Available)")
+        print("D: Extra Die (off) (Not Available)")
+        print("------ Sound Settings ------")
+        print("S: Sound Effect Toggle (on) (Not Available)")
+        print("T: TTS Audio Toggle (off) (Not Available)")
+
 
         option = input("Type an option: ").lower()
 
@@ -21,6 +30,20 @@ def main_menu():
         elif option == "2":
             total_players = 2
             player_name(total_players)
+        
+        # Three Players Mode
+        elif option == "3":
+            total_players = 3
+            player_name(total_players)
+
+        # Four Players Mode
+        elif option == "4":
+            total_players = 4
+            player_name(total_players)
+
+        # Five or More Players Mode
+        elif option == "5":
+            pass
 
         # Sound Toggle
         elif option == "s":
@@ -73,10 +96,42 @@ def player_name(total_players):
                 print("Please enter 'yes' or 'no'")
 
         if total_players == len(names):
-            print("good")
-        print(names)
+            print("Game will now start!")
+            start_game(names, total_players)
+        # print(names)
 
 
-# player_name()
+def roll_die():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    print(roll)
+
+
+
+def start_game(names, total_players):
+    # Adding Each Name to a List for Greeting Message
+    name_list = []
+    for name in names.values():
+        name_list.append(name)
+    
+    # Joining Each Name Separated by a ','
+    greeting = ", ".join(name_list)
+    print(f"Hello {greeting} have fun with this game! All the best!")
+
+
+    # Determine Who Starts First
+    for x in range(total_players):
+        while True:
+            print("\nHighest roll starts the game first!")
+            game_position = input(f"Roll the Die {name_list[x]} (y): ")
+            if game_position == "y":
+                roll_die()
+                break
+            else:
+                print("You must roll the die!")
+
 
 main_menu()
+# roll_die()
