f7eb1f759dcdd238423da7876bc886dba432f136
Uzy777 <usmon.h@outlook.com>
2024-12-18 14:47:06 +0000

    Add comments

--- Pig Game/main.py
+++ Pig Game/main.py
@@ -5,9 +5,13 @@
 def player_name():
     while True:
         name = input("\nPlease input your name: ").title()
+
+        # Name Validation Checking
         if not name.isalpha():
             print("This is an invalid name. Try again!")
             continue
+        
+        # Name Confirmation Check (yes/no) with Validation
         name_confirmation = input(f"You have selected {name} as your name. Is this correct? (yes/no): ").lower()
         if name_confirmation in ["yes", "y"]:
             print("yes")
