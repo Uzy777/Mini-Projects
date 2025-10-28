3c9e13d5dcb1e5c020057272c3ca4ee611bbf319
Uzy777 <usmon.h@outlook.com>
2025-10-28 20:39:52 +0000

    basic structure with react + vite + tailwind implemented

---
+++ React/typing-game/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css"; // Tailwind styles
+import { useState } from "react"; // optional if you need state
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Hello, World! \U0001f44b</h1>
+    </div>
+  );
+}
+
+export default App;
