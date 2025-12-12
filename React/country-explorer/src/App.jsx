4a2ee85fdb7e287226597dfbad35a7ed53f9077e
Uzy777 <usmon.h@outlook.com>
2025-12-12 12:56:30 +0000

    country-explorer: Data passed through using map

--- React/country-explorer/src/App.jsx
+++ React/country-explorer/src/App.jsx
@@ -5,12 +5,47 @@ import "./App.css";
 
 import CountryCard from "./components/CountryCard";
 
+const countries = [
+    {
+        name: "Japan",
+        flag: "https://flagcdn.com/w320/jp.png",
+        region: "Asia",
+        capital: "Tokyo",
+        population: "125800000",
+    },
+    {
+        name: "Germany",
+        flag: "https://flagcdn.com/w320/de.png",
+        region: "Europe",
+        capital: "Berlin",
+        population: "83019200",
+    },
+    {
+        name: "Brazil",
+        flag: "https://flagcdn.com/w320/br.png",
+        region: "Americas",
+        capital: "Brasila",
+        population: "211050000",
+    },
+];
+
 function App() {
     return (
         <>
             <h1>Country Explorer</h1>
             <h2>Browse and learn about countries around the world!</h2>
-            <CountryCard name={"Japan"} flag={"japan-flag-url"} region={"Asia"} capital={"2939239"} population={"1 Million"} />
+            <div>
+                {countries.map((country) => (
+                    <CountryCard
+                        key={country.name}
+                        name={country.name}
+                        flag={country.flag}
+                        region={country.region}
+                        capital={country.capital}
+                        population={country.population}
+                    />
+                ))}
+            </div>
         </>
     );
 }
