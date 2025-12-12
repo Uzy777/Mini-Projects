4f5bb70cac5ac63932a235606a9fa45110a9b5f9
Uzy777 <usmon.h@outlook.com>
2025-12-12 23:43:15 +0000

    country-explorer: Added search by coutnry functionality

--- React/country-explorer/src/App.jsx
+++ React/country-explorer/src/App.jsx
@@ -5,6 +5,9 @@ import CountryCard from "./components/CountryCard";
 
 function App() {
     const [countries, setCountries] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const filteredCountries = countries.filter((country) => country.name.common.toLowerCase().includes(searchQuery.toLowerCase()));
 
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region")
@@ -22,11 +25,21 @@ function App() {
             <h1 className="text-4xl text-center font-bold mt-8">Country Explorer</h1>
             <h2 className="text-xl text-center text-gray-700">Browse and learn about countries around the world!</h2>
 
+            <div className="flex justify-center pt-5">
+                <input
+                    type="text"
+                    placeholder="Search countries"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="broder p-2 text-center"
+                ></input>
+            </div>
+
             {countries.length === 0 ? (
                 <h2>Loading...</h2>
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                    {countries.map((country) => (
+                    {filteredCountries.map((country) => (
                         <CountryCard
                             key={country.name.common}
                             name={country.name.common}
