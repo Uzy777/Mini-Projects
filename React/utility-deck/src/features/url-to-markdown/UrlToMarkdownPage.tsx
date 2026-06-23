de5653ebb83cbad19d67d1366c83414bda989ef3
Uzy777 <usmon.h@outlook.com>
2026-06-23 19:26:00 +0100

    utility-deck: added base folder structure and initial starter files

---
+++ React/utility-deck/src/features/url-to-markdown/UrlToMarkdownPage.tsx
@@ -0,0 +1,13 @@
+import "./UrlToMarkdownPage.css";
+
+function UrlToMarkdownPage() {
+    return (
+        <main>
+            <h1>Utility Deck</h1>
+
+            <p>Simple and reliable tools for everyday tasks and conversions.</p>
+        </main>
+    );
+}
+
+export default UrlToMarkdownPage;
