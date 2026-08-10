ef8f2192031f68b14ca4541b5c5a70ee250eb67e
Uzy777 <usmon.h@outlook.com>
2026-08-10 20:45:36 +0100

    no-more-later: updated the top navbar menu to match the theme

--- React/no-more-later/app/(tabs)/journeys/_layout.tsx
+++ React/no-more-later/app/(tabs)/journeys/_layout.tsx
@@ -1,8 +1,24 @@
 import { Stack } from "expo-router";
 
+import { colours } from "@/constants/design";
+
 export default function JourneysLayout() {
     return (
-        <Stack>
+        <Stack
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: colours.surface,
+                },
+
+                headerTintColor: colours.text,
+
+                headerTitleStyle: {
+                    fontWeight: "700",
+                },
+
+                headerShadowVisible: false,
+            }}
+        >
             <Stack.Screen
                 name="index"
                 options={{
